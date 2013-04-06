6ab97e5a6e4fa99b7abf1e85e11c5320f9b42e58
Hans Petter Langtangen <hpl@simula.no>
2013-04-06 12:41:44 +0200

    updates

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from distutils.core import setup
+
+# Installation of just one module (no package)
 setup(name='DrawFunction',
       version='0.1',
-      package_dir={'': 'DrawFunction'}
+      author'Hans Petter Langtangen <hpl@simula.no>',
+      package_dir={'': 'src'},     # needed if module is not in current dir
       py_modules=['DrawFunction'],
       )
